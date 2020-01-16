59a2bff996f1d7e4c883aadc391f7982f0292469
Patrick Baker <bakerpj1992@gmail.com>
2020-01-15 23:25:38 -0600

    create api query retrieving information in XML format

--- server/routes/books.router.js
+++ server/routes/books.router.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+// allows access to dotenv file for api key
+require('dotenv').config();
+// when true, console logs show up in console.
+const verbose = true;
+// var stringify = require('json-stringify-safe');
+// var convert = require('xml-js');
+const xmlparser = require('express-xml-bodyparser');
 
 // GET route template for API request for book search
-router.get('/', (req, res) => {
-    
+router.get('/', xmlparser({trim: false, explicitArray: false}), (req, res) => {
+    if (verbose) console.log ('req.params of /books get:', req.params);
+    if (verbose) console.log ('req.body of /books get:', req.body);
+    axios.get('https://www.goodreads.com/search', {
+        params: {
+            q: req.body.queryText ,
+            key:process.env.GOODREADS_KEY  
+        }
+    }).then((result) => {
+        if (verbose) console.log('results of /books get:', result);
+        res.send(result);
+    }).catch( error => {
+        if (verbose) console.log('error in /books get request', error);
+        res.sendStatus(500);
+    })
 }) // end GET route
 
 module.exports = router;
