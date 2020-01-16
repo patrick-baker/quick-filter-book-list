db8963a95014e925507e250c67945a156f580a91
Patrick Baker <bakerpj1992@gmail.com>
2020-01-15 22:19:22 -0600

    first commit, express server connected to react boilerplate

---
+++ server/routes/books.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// GET route template for API request for book search
+router.get('/', (req, res) => {
+    
+}) // end GET route
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+// brings in bodyParser to allow axios requests from the front-end
+const bodyParser = require('body-parser');
+const books = require('./routes/books.router.js');
+const PORT = process.env.PORT || 5000;
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json()); // needed for axios requests
+app.use(express.static('build'));
+
+/** ---------- EXPRESS ROUTE ---------- **/
+app.use('/books', books);
+
+/** ---------- START SERVER ---------- **/
+app.listen(PORT,  () => {
+    console.log('Listening on port: ', PORT);
+});
